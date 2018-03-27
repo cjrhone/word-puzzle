cd1f49afdc83e1cd063efb2de22fe0b469152bb7
Alex Noble and CJ Rhone <cj@>
2018-03-27 09:06:49 -0700

    Add puzzle fuctionality

---
+++ js/scripts.js
@@ -0,0 +1,36 @@
+var sentence;
+var letters=[];
+var sortedLetters=[];
+var vowels =["a", "e", "i","o","u","y" ];
+var newSentence="";
+
+var found=0;
+
+$(document).ready(function() {
+  $("button#add").click(function(){
+    sentence=$("#item1").val();
+    letters=sentence.split("");
+
+    
+    $(".result").fadeIn();
+
+    letters.forEach (function(letter){
+      found=0;
+      vowels.forEach (function(vowel){
+        if (letter.toString()===vowel.toString()){
+          newSentence+="-";
+          found++;
+        }
+        else{}
+
+      });
+
+      if (found===0)
+      {
+        newSentence+=letter;
+      } else{}
+
+    });
+    alert(newSentence);
+  });
+});
